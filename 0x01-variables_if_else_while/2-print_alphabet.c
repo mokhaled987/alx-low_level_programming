ddf21274f2f35df463e17eebb2a99d1324c45420
mokhaled987 <mohammedkhaled46112@gmail.com>
2023-07-14 18:24:28 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Determine if a random number is positive, negative or zero.
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 on (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while {ch <= 'z'}
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+
+
